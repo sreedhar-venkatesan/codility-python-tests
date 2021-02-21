f3aea450f3feebb52b3b458fe2dfebdf3585e3af
sreedhar venkatesan <sreevenk@volvocars.com>
2021-02-21 15:19:04 +0100

    one liner

--- codility_python_tests/05_max_gaps_binary.py
+++ codility_python_tests/05_max_gaps_binary.py
@@ -1,15 +1,5 @@
 def solution(N):
-    bin = "{0:b}".format(N)
-    max_gap = 0
-    accum = 0
-    for i in bin:
-        if i=='0':
-            accum += 1
-        elif i =='1':
-            if accum> max_gap:
-                max_gap =accum
-            accum = 0
-    return (max_gap)
+    return len(max(format(N,'b').strip('0').split('1')))
 
 if __name__ =="__main__":
     X= solution(20)
