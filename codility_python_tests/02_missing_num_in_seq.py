1a4238e13e91bd08ba64e35f023e930f7b6f5396
sreedhar venkatesan <sreevenk@volvocars.com>
2021-02-21 14:39:55 +0100

    onliner answer

--- codility_python_tests/02_missing_num_in_seq.py
+++ codility_python_tests/02_missing_num_in_seq.py
@@ -1,15 +1,7 @@
 def solution(A):
-    bool = [False]*len(A)
-
-    for i in A:
-        if 0<=i> len(A):
-            return 0
-        if bool[i-1] == True:
-            return 0
-        bool[i-1] = True
-    return 1
+    return 1 if set(A)==set(range(1,len(A))) else 0
 
 if __name__ == "__main__":
-    A = [1,2,3,4]
+    A = [1,2,3,4,2]
     X=solution(A)
     print (X)
