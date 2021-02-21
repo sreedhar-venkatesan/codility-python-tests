b9536eb1bced90ba9a5a192d3cc424be4367b406
sreedhar venkatesan <sreevenk@volvocars.com>
2021-02-21 19:46:34 +0100

    minor updates

--- coditlity_test_210221/task_2.py
+++ coditlity_test_210221/task_2.py
@@ -1,6 +1,10 @@
+## Task 2
+
+
 def solution(A):
+    "get the max sum of 2 numbers"
     A.sort()
-    B= A[-1]+A[-2]
+    B = A[-1]+A[-2]
     max_A = [int(digit) for digit in str(A[-1])]
     sec_max_A = [int(digit) for digit in str(A[-2])]
     digit_sum =[]
@@ -14,7 +18,10 @@ def solution(A):
         return B
     else:
         return -1
+
+
 if __name__ == "__main__":
+    "main function"
     A = [10,21,33]
     X = solution(A)
     print (X)
