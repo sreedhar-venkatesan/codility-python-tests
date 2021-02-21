b9dcf0da33172befd26a9998e1228a0ae65dd69d
sreedhar venkatesan <sreevenk@volvocars.com>
2021-02-21 14:51:45 +0100

    right answer 1

--- codility_python_tests/03_odd_occurence_in_array.py
+++ codility_python_tests/03_odd_occurence_in_array.py
@@ -1,11 +1,19 @@
 def solution(A):
-    for num in A:
-        if A.count(num)%2:
-            return num
+    A.sort()
+    N = len(A)
+    i=0
+    while i<N:
+        try:
+            if A[i] ==A[i+1]:
+                i+=2
+            else:
+                return A[i]
+        except:
+            return A[N-1]
 
 
 
 if __name__ == "__main__":
-    A = [1,2,1,2,1,2,1,2,5]
+    A = [1,2,1,2,1,4,2,1,2,5]
     X=solution(A)
     print (X)
