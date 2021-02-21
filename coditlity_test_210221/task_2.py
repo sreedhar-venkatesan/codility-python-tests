932603d31e554ffeffe7118440e56fbcf242cdd0
sreedhar venkatesan <sreevenk@volvocars.com>
2021-02-21 19:27:46 +0100

    coding test 2 updated for max sum

---
+++ coditlity_test_210221/task_2.py
@@ -0,0 +1,20 @@
+def solution(A):
+    A.sort()
+    B= A[-1]+A[-2]
+    max_A = [int(digit) for digit in str(A[-1])]
+    sec_max_A = [int(digit) for digit in str(A[-2])]
+    digit_sum =[]
+    for i in range(len(max_A)):
+        digit_temp= str(max_A[i] + sec_max_A[i])
+        digit_sum.append(digit_temp)
+    digit_sum_final =""
+    for i in digit_sum:
+        digit_sum_final += i
+    if int(digit_sum_final) == B:
+        return B
+    else:
+        return -1
+if __name__ == "__main__":
+    A = [10,21,33]
+    X = solution(A)
+    print (X)
