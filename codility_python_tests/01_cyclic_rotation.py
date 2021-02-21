6452f2a936b90c9b3c6b4f90a8ec398438ed97d7
sreedhar venkatesan <sreevenk@volvocars.com>
2021-02-21 14:29:40 +0100

    right answer 2

--- codility_python_tests/01_cyclic_rotation.py
+++ codility_python_tests/01_cyclic_rotation.py
@@ -1,11 +1,11 @@
 def solution(A,K):
-    N=len(A)
-    B=[0]*N
-    for i in range(N):
-        B[i]=A[(i-K)%N]
-    return B
+    if len(A) == 0:
+        return A
+    K=K%len(A)
+    return A[-K:] + A[:-K]
 if __name__ =="__main__":
     K=3
     A=[3,8,9,7,6,8]
     A=solution(A,K)
     print (A)
+ 
